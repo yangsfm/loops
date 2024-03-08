ee2cefcff3efd41557c0a5ddb574c3fae18cf99a
Yang Sarmento Ferreira <60950025+yangsfm@users.noreply.github.com>
2024-03-07 21:26:05 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+ // while = enquanto
+
+//  var x = 5;
+//  while(x < 10){
+//     document.write("<br> O valor do x é:" + x);
+
+//     x++;
+//  } 
+
+ //For = para
+ var valor = 30;
+
+ for(a = 0; a < valor; a++){
+
+    document.write("<br> O valor de a é:" + a)
+    console.log(a + 10);
+    
+ }
